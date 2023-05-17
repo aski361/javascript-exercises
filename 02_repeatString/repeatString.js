68a2aec286a05c2cc2db74b3c36b38d549ff2825
aski361 <karpowiczaskaniusz@outlook.com>
2023-05-17 19:44:35 +0200

    exercise finished

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+    let fullString = '';
+  for (let i=0;i<n;i++) {
+    fullString += string;
+  }
+  return fullString;
 };
+
+console.log(repeatString('hey',3))
 
 // Do not edit below this line
 module.exports = repeatString;
