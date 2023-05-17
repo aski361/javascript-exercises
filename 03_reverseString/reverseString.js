61ed3c68e7aab2aa27f246a0b22a8453dfc14a26
aski361 <karpowiczaskaniusz@outlook.com>
2023-05-17 19:54:50 +0200

    exercise 03_reverseString done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let strLen = string.length-1;
+    let reverseString = '';
+    for(i=strLen;i>=0;i--) {
+        reverseString += string[i];
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
