ab7d545ebf9f0c67462f32cac21137e68896e6d3
aski361 <karpowiczaskaniusz@outlook.com>
2023-05-18 19:08:25 +0200

    exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = null;
+    for(i=0;i<=b;i++) {
+        sum +=i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
