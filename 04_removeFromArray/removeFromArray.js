488bd4d80628db47b18d2cbc404b6e9f67759b64
aski361 <karpowiczaskaniusz@outlook.com>
2023-05-18 19:04:22 +0200

    execise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, n) {
+    let newArray = array.splice(n-1,1);
+    return array;
 };
 
 // Do not edit below this line
