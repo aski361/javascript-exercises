3dc0e9256a7570808c1d9b2ed0be2ff2c9b8ea69
aski361 <karpowiczaskaniusz@outlook.com>
2023-05-28 11:34:38 +0200

    done exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strNew = str.toLowerCase();
+    let strBack = str.split('').reverse().join('').toLowerCase();
+    for(let i=0;i<str.length;i++) {
+        if(strBack[i] == strNew[i]) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 };
-
-// Do not edit below this line
-module.exports = palindromes;
+  
+  module.exports = palindromes;
+  
