070a3a64cc4d40df9b146e925b285055a0288a71
aski361 <karpowiczaskaniusz@outlook.com>
2023-05-29 13:54:03 +0200

    10_fibonacci exercise done

--- 10_fibonacci/solution/fibonacci-solution.js
+++ 10_fibonacci/solution/fibonacci-solution.js
@@ -1,14 +1,18 @@
-const fibonacci = function (count) {
-  if (count < 0) return "OOPS";
-  if (count === 0) return 0;
-  let a = 0;
-  let b = 1;
-  for (let i = 1; i < count; i++) {
-    const temp = b;
-    b = a + b;
-    a = temp;
-  }
-  return b;
+const fibonacci = function(number) {
+    if (number==0) {
+        return 0
+    } else if (number==1) {
+        return 1;
+    } else if (number>1) {
+        let a = 0;
+        let b = 1
+        for(let i=1;i<number;i++) {
+            const temp = b;
+            b = a + b;
+            a = temp;
+        }
+        return b
+    }
 };
 
 module.exports = fibonacci;
